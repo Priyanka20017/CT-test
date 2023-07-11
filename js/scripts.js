deb417be4f77739c417b99caf3cf3236a42cccbf
Priyanka20017 <49614509+Priyanka20017@users.noreply.github.com>
2023-07-11 12:56:09 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+// Anchor function
+function scrollHeader() {
+    let e = document.getElementById('header');
+    e.scrollIntoView();
+}
+
+function scrollHistory() {
+    let e = document.getElementById('history');
+    e.scrollIntoView();
+}
+
+function scrollTeam() {
+    let e = document.getElementById('team');
+    e.scrollIntoView();
+}
